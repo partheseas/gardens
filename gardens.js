2dee4fa2f34c2c27a831f404873efad37149c8ae
Kayla <tylermwashburn@gmail.com>
2017-11-23 19:21:49 -0700

    Steal things I like from the debug package :)

--- gardens.js
+++ gardens.js
@@ -7,11 +7,12 @@ let fs = require( 'fs' )
 let util = require( 'util' )
 
 let chalk = require( 'chalk' )
+let supportsColor = require( 'supports-color' )
 
 let configuration = {}
 
 class Timer {
-  constructor( garden, name = 'anonymous' ) {
+  constructor( garden, name ) {
     this.garden = garden
     this.name = name
 
@@ -27,7 +28,7 @@ class Timer {
     // Pad with the appropriate amount of zeros for logging
     ns = '0'.repeat( 9 - ns.toString().length ) + ns
 
-    print( this.garden.name, `Timer:${this.name.toString()}`, `took ${s}.${ns} seconds` )
+    print( this.garden, `Timer:${this.name.toString()}`, `took ${s}.${ns} seconds` )
   }
 }
 
@@ -40,6 +41,13 @@ module.exports = class Garden extends events.EventEmitter {
     this.verbose = verbose
     this._times = {}
     this._counts = {}
+
+    this.color = 0
+    for ( var i in name ) this.color += name.charCodeAt( i )
+    this.color %= 199
+    this.color += 25
+
+    console.log( chalk.ansi256( this.color )( this.name, this.color ) )
   }
 
   static configure( update ) {
@@ -62,7 +70,7 @@ module.exports = class Garden extends events.EventEmitter {
   debug( message, ...extra ) {
     this._emit( 'debug', message, ...extra )
     if ( this.isVerbose() ) {
-      print( this.name, 'Debug', format( message ), extra )
+      print( this, 'Debug', message, extra )
       return true
     }
     return false
@@ -72,7 +80,7 @@ module.exports = class Garden extends events.EventEmitter {
     let error = new Error( message )
     this._emit( 'trace', error, ...extra )
     if ( this.isVerbose() ) {
-      print( this.name, 'Trace', format( message ), extra )
+      print( this, 'Trace', message, extra )
       console.log( error.stack )
       return true
     }
@@ -81,7 +89,7 @@ module.exports = class Garden extends events.EventEmitter {
 
   log( message, ...extra ) {
     this._emit( 'log', message, ...extra )
-    print( this.name, 'Log', format( message ), extra )
+    print( this, 'Log', message, extra )
   }
 
   info( ...details ) {
@@ -90,7 +98,7 @@ module.exports = class Garden extends events.EventEmitter {
 
   warning( message, ...extra ) {
     this._emit( 'warning', message, ...extra )
-    print( this.name, 'Warning', chalk.yellow( format( message ) ), extra )
+    print( this, 'Warning', chalk.yellow( message ), extra )
   }
 
   warn( ...details ) {
@@ -100,7 +108,7 @@ module.exports = class Garden extends events.EventEmitter {
   error( message, ...extra ) {
     let error = new Error( message )
     this._emit( 'error', error, ...extra )
-    print( this.name, 'Error', chalk.red( format( message ) ), extra )
+    print( this, 'Error', chalk.red( message ), extra )
     console.log( error.stack )
     return error
   }
@@ -108,7 +116,7 @@ module.exports = class Garden extends events.EventEmitter {
   typeerror( message, ...extra ) {
     let error = new TypeError( message )
     this._emit( 'typeerror', error, ...extra )
-    print( this.name, 'TypeError', chalk.red( format( message ) ), extra )
+    print( this, 'TypeError', chalk.red( message ), extra )
     console.log( error.stack )
     return error
   }
@@ -116,7 +124,7 @@ module.exports = class Garden extends events.EventEmitter {
   referenceerror( message, ...extra ) {
     let error = new ReferenceError( message )
     this._emit( 'referenceerror', error, ...extra )
-    print( this.name, 'ReferenceError', chalk.red( format( message ) ), extra )
+    print( this, 'ReferenceError', chalk.red( message ), extra )
     console.log( error.stack )
     return error
   }
@@ -124,12 +132,12 @@ module.exports = class Garden extends events.EventEmitter {
   catch( error, ...extra ) {
     if ( !error.stack ) error = new Error( error )
     this._emit( 'catch', error, ...extra )
-    print( this.name, 'Caught Error', `${chalk.red( error.name )}: ${error.message}`, extra )
+    print( this, 'Caught Error', `${chalk.red( error.name )}: ${error.message}`, extra )
     console.log( error.stack )
     return error
   }
 
-  time( name ) {
+  time( name = 'anonymous' ) {
     return this._times[ name ] = new Timer( this, name )
   }
 
@@ -140,23 +148,36 @@ module.exports = class Garden extends events.EventEmitter {
 
   count( name = 'anonymous' ) {
     if ( !this._counts[ name ] ) this._counts[ name ] = 0
-    print( this.name, `Count:${name.toString()}`, ++this._counts[ name ] )
+    print( this, `Count:${name.toString()}`, ++this._counts[ name ] )
   }
 }
 
-let format = function ( message ) {
-  return typeof message === 'string' ? message : util.inspect( message )
+let formats = module.exports.formats = {
+  O: O => util.inspect( O ),
+  o: o => util.inspect( o ).replace(/\s*\n\s*/g, ' '),
+  
 }
 
-let print = function ( name, type, message, extra ) {
-  process.stdout.write( `[${name}] ${chalk.cyan( `[${type}]` )}  ${message} ` )
+let pretty = function ( message, format = 'O' ) {
+  if ( typeof formats[ format ] !== 'function' ) return console.error( new TypeError( `Invalid format '${format}'!` ) )
+  return typeof message === 'string' ? message : formats[ format ]( message, { colors: true } )
+}
+
+let print = function ( garden, type, message, extra ) {
+  if ( typeof message === 'string' ) message = message.replace( /%([A-Za-z%])?/g, ( _, format ) => {
+    if ( extra.length ) return format === '%' ? '%' : pretty( extra.shift(), format )
+    return _
+  } )
+
+  process.stdout.write( `${chalk.ansi256( garden.color )( `[${garden.name}][${type}]` )}  ${pretty( message ) }` )
+  extra.forEach( each => process.stdout.write( ' ' + pretty( each ) ) )
 
   if ( configuration.outputPath ) fs.appendFile(
     configuration.outputPath,
-    `[${new Date().toLocaleString()}]  [${name}] [${type}]  ${message}\n`,
+    `[${new Date().toLocaleString()}][${name}][${type}]  ${pretty( message )}\n`,
     'utf-8',
-    ( error ) => { if ( error ) console.error( error ) }
+    error => { if ( error ) console.error( error ) }
   )
 
-  extra && extra.length ? console.log( ...extra ) : process.stdout.write( '\n' )
+  process.stdout.write( '\n' )
 }
